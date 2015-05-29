5f4fba4394d351b01e9a78ca9e27bbf6a497dcd2
Anselmo Fresquez <anselmofresquez@Anselmo-Fresquezs-iMac.local>
2015-05-29 06:57:27 -0700

    Added random methods.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
-alert("Greetings.");
-document.write("Benedict Cumberbatch.");
-alert("We will fight in the shade.");
+Greetings();
+
+function MyFirstProgram() { 
+  alert("Greetings.");
+  document.write("Benedict Cumberbatch.");
+  alert("We will fight in the shade.");
+}
+
+function Arg(message) {
+  alert(message);
+}
+
+function Parrot() {
+  Arg(prompt("Say what?").toUpperCase() + "!!!");
+}
+
+function Greetings(){
+  var greeting = "Hello, " + prompt("What is your name?") + ".";
+  greeting += " It's so very nice to have a guest.";
+  greeting += " It's been so long since we've had guests, here.";
+  document.write(greeting);
+}
